1c5e184a6e97b09d5a2fef3627383f4e4f33070c
Mihai Popa <mihai.119.popa@gmail.com>
2026-04-05 16:04:04 +0300

    launch file for turtlebot3 world

---
+++ src/basic_slam/launch/slam_turtlebot3_world_launch.py
@@ -0,0 +1,49 @@
+import os
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    rviz_config = os.path.join(get_package_share_directory('basic_slam'), 'config', 'slam_config.rviz')
+    turtlebot3_world = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'turtlebot3_world.launch.py')
+
+    model_env = SetEnvironmentVariable(
+        name='TURTLEBOT3_MODEL',
+        value='burger'
+    )
+
+    imu_odom_node = Node(
+        package='basic_slam',
+        executable='imu_odom',
+        name='imu_odom'
+    )
+
+    map_node = Node(
+        package='basic_slam',
+        executable='map_node',
+        name='map_node'
+    )
+    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        output='screen'
+    )
+
+    gazebo = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+        turtlebot3_world
+    )
+)
+
+    return LaunchDescription([
+        model_env,
+        imu_odom_node,
+        map_node,
+        rviz_node,
+        gazebo,
+    ])
